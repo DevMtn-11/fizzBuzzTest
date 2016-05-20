ac80f9ccbd1a62a73db2531e576d267b732a953e
Joseph Toh Djojo <joseph.tohdjojo@gmail.com>
2016-05-20 10:21:57 -0600

    completed

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
 // write a program called fizzBuzz that console.logs numbers from 1 to 100, but for multiples of three print 'fizz,' for multiples of five print 'buzz,' and for multiples of both three and five print 'fizzbuzz.' Otherwise, do nothing.
+
+
+function fizzBuzz() {
+  for(var i = 1; i < 100; i++) {
+    var fb = '';
+
+    if(i % 3 === 0) {
+      fb += 'fizz';
+    }
+    if(i % 5 === 0) {
+      fb += 'buzz';
+    }
+    
+    if(fb !== '') {
+      console.log(fb);
+    }
+    else {
+      console.log(i);
+    }
+  }
+}
